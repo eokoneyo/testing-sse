9b80613b93e230032a5a66ddf1d7fd8d26975b42
Eyo O. Eyo <maziey93@gmail.com>
2019-03-20 17:00:34 +0100

    add CORS support for development

--- app/server.js
+++ app/server.js
@@ -5,6 +5,17 @@ const path = require('path');
 
 const app = express();
 
+// Add CORS support for development
+if (process.env.NODE_ENV === 'development' || typeof process.env.NODE_ENV === 'undefined') {
+    app.use((req, res, next) => {
+        res.setHeader('Access-Control-Allow-Origin', req.get('Origin'));
+        res.setHeader('Access-Control-Allow-Methods', 'POST,GET,OPTIONS,PUT,DELETE');
+        res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Accept');
+        res.setHeader('Access-Control-Allow-Credentials', true);
+        next();
+    });
+}
+
 // static files
 app.use(express.static(path.join('app/static')));
 
